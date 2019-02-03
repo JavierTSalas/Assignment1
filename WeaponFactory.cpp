f4a9c80c718983c7664af9f9443fba675852d746
Javier Salas <jtomassalas97@gmail.com>
2019-02-03 14:24:27 -0500

    Added new weapons to WeaponFactory (I'm commit whole files after development - this should probably have been done iteratively)

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,8 +9,12 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "CrazyRandomSword.h"
+#include "SimpleHammer.h"
+#include "FiftyFifty.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
+
 
 WeaponFactory * WeaponFactory::getInstance() {
     if (!instance) {
@@ -23,9 +27,21 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("sword") == 0) {
         return new CommonSword();
     }
-    
+
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+
+    if (name.compare("crazy") == 0) {
+        return new CrazyRandomSword();
+    }
+
+    if (name.compare("hammer") == 0) {
+        return new SimpleHammer();
+    }
+
+    if (name.compare("fifty") == 0) {
+        return new FiftyFifty();
     }
 
     throw "Invalid weapon";
